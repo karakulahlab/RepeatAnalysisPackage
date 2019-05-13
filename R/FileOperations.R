d3e83d0731b751aec94e418dbcedd2157588f773
nzmye <nzmye.arslan01@gmail.com>
2019-05-13 17:23:20 +0300

    deleted data source, instead of it added download file from google drive

--- R/FileOperations.R
+++ R/FileOperations.R
@@ -20,10 +20,10 @@ setHeadrs <- function(df,vectHeaders){ #to set header to columns
 # library(biomartr)
 # #https://drive.google.com/open?id=14-CbLlCgnYRr_GX6jiu-Ixn-5MziNcnQ
 #
-# readRepeatMasker<-function(assembly){
-#   datatable(readr::read_csv("https://docs.google.com/spreadsheets/d/1f98kFdj9mxVDc1dv4xTZdx8iWgUiDYO-qiFJINvmTZs/export?format=csv&gid=2046985454"),
-#             filter = 'top',
-#             options = list(scrollX = TRUE, keys = TRUE, pageLength = 40),
-#             rownames = FALSE)
-#
-# }
+readRepeatMasker<-function(assembly){
+library(googledrive)
+ dt<-drive_download(
+  as_id("14-CbLlCgnYRr_GX6jiu-Ixn-5MziNcnQ"), type = ".csv", overwrite = TRUE)
+ dt<-read.csv(dt$local_path,sep = "\t")
+ return(dt)
+}
